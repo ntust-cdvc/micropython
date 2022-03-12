1ead8e0b845383902f3bc1d4cb25c557097405da
Woodrow Shen <woodrow.shen@gmail.com>
2022-03-12 13:37:33 +0800

    Add files via upload

---
+++ play-turtle.py
@@ -0,0 +1,14 @@
+# Write your code here :-)
+import turtle
+
+海龜畫筆 = turtle.Pen()
+
+# 往前移動50步
+海龜畫筆.forward(50)
+海龜畫筆.left(90)
+海龜畫筆.forward(50)
+海龜畫筆.left(90)
+海龜畫筆.forward(50)
+海龜畫筆.left(90)
+海龜畫筆.forward(50)
+
